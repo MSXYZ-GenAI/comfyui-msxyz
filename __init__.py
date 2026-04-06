0c94db1a3f91668253839443ddb8a27dfe6ac24d
MSXYZ <msahin@berksan.com>
2026-04-07 01:15:08 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from .AdvancedColorAdjustment import AdvancedColorAdjustment
-
-NODE_CLASS_MAPPINGS = {
-    "AdvancedColorAdjustment": AdvancedColorAdjustment
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "AdvancedColorAdjustment": "Color Correction & Adjustment"
-}
