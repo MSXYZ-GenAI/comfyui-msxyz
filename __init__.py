611bf66d3906c60313f05c660214726f4e28f8c1
MSXYZ <msahin@berksan.com>
2026-04-07 01:15:42 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .LightExplosion import LightExplosion
+
+NODE_CLASS_MAPPINGS = {
+    "LightExplosion": LightExplosion
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LightExplosion": "Color Correction & Adjustment"
+}
