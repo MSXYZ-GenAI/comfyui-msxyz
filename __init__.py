7b1765b4696d1a6cfa4dbd84f7fb98854a861d99
MSXYZ <msahin@berksan.com>
2026-04-07 09:09:01 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from .LightExplosion import LightExplosion
-
-NODE_CLASS_MAPPINGS = {
-    "LightExplosion": LightExplosion
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "LightExplosion": "Color Correction & Adjustment"
-}
