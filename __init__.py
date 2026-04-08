8e4708abaef21571bdcdfbd0e0dfc956b4260629
MSXYZ <msahin@berksan.com>
2026-04-08 15:14:32 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,3 @@
-from .VideoAdaptiveAA import VideoAdaptiveAA
-
-NODE_CLASS_MAPPINGS = {
-    "VideoAdaptiveAA": VideoAdaptiveAA
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "VideoAdaptiveAA": "🚀 Video Adaptive Anti-Aliasing"
-}
-
-
 from .video_aa_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
